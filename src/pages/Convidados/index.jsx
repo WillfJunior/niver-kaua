10ffbdb112ee81d97808c9b652a9251f6474bd02
willfjunior <wfrutuosojunior@gmail.com>
2023-10-16 18:50:38 -0300

    adjusts

--- src/pages/Convidados/index.jsx
+++ src/pages/Convidados/index.jsx
@@ -1,44 +1,75 @@
+import { useState } from "react"
 import  "./style.css"
-import { TextField, Button } from "@mui/material"
+import { TextField, Button, Stack,Alert } from "@mui/material"
 export default function Convidados() {
+    const [nome, setNome] = useState('');
+    const [qtdAdultos, setQtdAdultos] = useState(0);
+    const [qtdCriancas, setQtdCriancas] = useState(0);
+    const [confirmacao, setConfirmacao] = useState(false);
+
+    function handleConfirmation() {
+        
+        console.log(nome, qtdAdultos, qtdCriancas);
+        setConfirmacao(true);
+    }
     return (
         <>
-            <h1>Você está Convidado!!!</h1>
-            <div className="container-convidados">
-               
-                <img src="/src/assets/kaua.png" alt="Convidados" width="40%" className="img-container" />
-               <div className='form-convidados'>
-                    <TextField
-                        id="outlined-required"
-                        label="Insira seu Nome"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        
-                    />
-                    <div className="form-convidados-qtds">
-                        <TextField
-                                id="outlined-number"
-                                label="Qtd Adultos"
-                                type="number"
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                />
-
-                        <TextField
-                                id="outlined-number"
-                                label="Qtd Crianças"
-                                type="number"
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                />
-                    </div>
-                    <Button variant="contained">Confirmar</Button>
-                </div>
+            <h2>Por favor confirme a sua presença e quantidade de pessoas da sua familia que estarão presentes.!!!</h2>
+            
+            <Stack  direction={{ xs: 'column', sm: 'row' }}
+                    spacing={{ xs: 1, sm: 2, md: 4 }} justifyContent="center" >
+                <img src="/src/assets/kauanovo.png" alt="Convidados" width="25%" height="40%" />
+                <Stack spacing={2} direction="column" justifyContent="center" >
                 
-            </div>
+                    {/* <img src="/src/assets/kaua1.png" alt="Convidados" width="20%" height="40%"  /> */}
+                    
+                                
+                                    <TextField
+                                            id="outlined-required"
+                                            label="Insira seu Nome"
+                                            InputLabelProps={{
+                                                shrink: true,
+                                            }}
+                                            value={nome}
+                                            onChange={(e) => setNome(e.target.value)}
+                                            
+                                    />
+                                
+                                
+                    <Stack spacing={2} direction="row">
+
+                            <TextField
+                                            id="outlined-number"
+                                            label="Qtd Adultos"
+                                            type="number"
+                                            InputLabelProps={{
+                                                shrink: true,
+                                            }}
+                                            value={qtdAdultos}
+                                            onChange={(e) => setQtdAdultos(e.target.value)}
+                                            />
+
+                            <TextField
+                                    id="outlined-number"
+                                    label="Qtd Crianças"
+                                    type="number"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    value={qtdCriancas}
+                                    onChange={(e) => setQtdCriancas(e.target.value)}
+                                    />
+                    </Stack>
+                    
+                    <Button variant="contained" onClick={handleConfirmation}>Confirmar</Button>
+                    {confirmacao && <Alert severity="success">Obrigado pela confirmação! Esperamos por vocês!</Alert>}
+                    
+                </Stack>
+                <img src="/src/assets/kaua1.png" alt="Convidados" width="25%" height="100%"  />
+            </Stack>
+            
+            
+            
         </>
     )
 }
